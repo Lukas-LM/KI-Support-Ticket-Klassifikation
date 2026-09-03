d8673376dbec06a8613b2fb209f519c3be8e8329
Lukas <marwede32@gmail.com>
2026-09-03 09:35:19 +0200

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -1,7 +1,7 @@
 from preprocessing import preprocessing
 import joblib
 from sqlalchemy import create_engine
-from functions import load_config, current_date
+from functions import load_config
 import pandas as pd
 from datetime import datetime
 import requests
